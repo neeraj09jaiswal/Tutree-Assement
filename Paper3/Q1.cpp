3a8277f06850f9a6e0ae6bb454569f66c88cad53
Neeraj Jaiswal <68476086+neeraj09jaiswal@users.noreply.github.com>
2021-01-22 19:30:03 +0530

    Create Q1.cpp

---
+++ Paper3/Q1.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int printUnion(int arr1[], int arr2[], int m, int n) 
+{ 
+    int i = 0, j = 0; 
+    cout<<"Union: ";
+    while (i < m && j < n) { 
+        if (arr1[i] < arr2[j]) 
+            cout << arr1[i++] << " "; 
+  
+        else if (arr2[j] < arr1[i]) 
+            cout << arr2[j++] << " "; 
+  
+        else { 
+            cout << arr2[j++] << " "; 
+            i++; 
+        } 
+    } 
+    while (i < m) 
+        cout << arr1[i++] << " "; 
+  
+    while (j < n) 
+        cout << arr2[j++] << " "; 
+    cout<<endl;
+}
+int printIntersection(int arr1[], int arr2[], int m, int n) 
+{ 
+    int i = 0, j = 0; 
+    cout<<"Intersection: ";
+    while (i < m && j < n) { 
+        if (arr1[i] < arr2[j]) 
+            i++; 
+        else if (arr2[j] < arr1[i]) 
+            j++; 
+        else
+        { 
+            cout << arr2[j] << " "; 
+            i++; 
+            j++; 
+        } 
+    } 
+} 
+int main() 
+{ 
+    int arr1[] = { 1, 3, 4, 5, 7 }; 
+    int arr2[] = { 2, 3, 5, 6 }; 
+  
+    int m = sizeof(arr1) / sizeof(arr1[0]); 
+    int n = sizeof(arr2) / sizeof(arr2[0]); 
+    printUnion(arr1, arr2, m, n);
+    printIntersection(arr1, arr2, m, n);
+  
+    return 0; 
+}
